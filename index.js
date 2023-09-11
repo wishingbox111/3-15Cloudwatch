c39a25abbd8424bcc984cdd3735374ab3db57666
wishingbox111 <wishingbox111@gmail.com>
2023-09-11 22:15:40 +0800

    add more codes

--- index.js
+++ index.js
@@ -50,3 +50,18 @@ lambdaHandler = async(event) => {
 }
 
 module.exports = { lambdaHandler, getGreeting }
+
+
+module.exports.handler = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v3.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  };
